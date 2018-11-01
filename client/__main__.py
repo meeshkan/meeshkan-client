39e9ddfc1067a541dd3266b6b3b25ba1e90d0141
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-11-01 10:30:09 +0200

    Fix pylint for __main__.

--- client/__main__.py
+++ client/__main__.py
@@ -1,24 +1,26 @@
+""" Command-line interface """
+import logging
+import click
+
 from client.config import get_config, get_secrets
 from client.oauth import TokenStore, token_source
 from client.notifiers import post_payloads, CloudNotifier
 from client.job import Job, ProcessExecutable
 
-import click
-import logging
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-config = get_config()
-secrets = get_secrets()
+CONFIG = get_config()
+SECRETS = get_secrets()
 
 
 def test_post():
-    auth_url = config['auth']['url']
-    client_id = secrets['auth']['client_id']
-    client_secret = secrets['auth']['client_secret']
+    auth_url = CONFIG['auth']['url']
+    client_id = SECRETS['auth']['client_id']
+    client_secret = SECRETS['auth']['client_secret']
     fetch_token = token_source(auth_url=auth_url, client_id=client_id, client_secret=client_secret)
     token_store = TokenStore(fetch_token=fetch_token)
-    post_payload = post_payloads(cloud_url=config['cloud']['url'], token_store=token_store)
+    post_payload = post_payloads(cloud_url=CONFIG['cloud']['url'], token_store=token_store)
     notifier = CloudNotifier(post_payload=post_payload)
     notifier.notify(Job(ProcessExecutable.from_str("echo hello"), job_id=10))
 
