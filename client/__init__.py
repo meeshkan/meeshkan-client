146297a7678ef23614bc51a4a41553a720f157ce
Idan Tene <idan.tene@gmail.com>
2018-11-02 16:51:24 +0200

    Initial version.
    Able to fetch globals() and locals() from another python process that also had `meeshkan_listener()` call in it.
    Depends on pickle to send variables. More experimenting with PyTorch and TF is required, to verify we can pull different data.

--- client/__init__.py
+++ client/__init__.py
@@ -7,3 +7,4 @@ import client.notifiers
 import client.oauth
 import client.scheduler
 import client.service
+
---
+++ client/stalker.py
@@ -0,0 +1,82 @@
+"""Module to enable remote querying of python processeses from outside the current process."""
+import signal
+import os
+import sys
+import pickle
+
+class PipedProcess(object):
+    """Opens a temporary named pipe to transmit information between process A and process B"""
+
+    def __init__(self, pid: int, reader: bool =False):
+        """Prepares the infrastructure for one-way information "pulling"
+
+        :param reader: Whether this class is instansiated from the reading process or the writing process
+        """
+        self.fname = os.path.abspath(".{}.pipe".format(pid))
+        try:
+            os.mkfifo(self.fname)  # Make a FIFO if possible (0 resources), otherwise we'll just have a regular file
+        except OSError:
+            pass
+        self.f = open(self.fname, 'rb' if reader else 'wb')  # Needs to be opened continuously otherwise pipe closes
+        self.is_reader = reader
+
+    def write(self, contents) -> bool:
+        """Writes `contents` to pipe. Assumes `contents` is pickleable."""
+        if self.is_reader:  # The reader-end of the pipe has no access to write!
+            return False
+        try:
+            self.f.write(pickle.dumps(contents))
+            self.f.flush()
+        except (OSError, TypeError):
+            return False
+        return True
+
+    def read(self):
+        """Attempts to read all contents from pipe. Assume the contents in the pipe was pickled.
+
+        :return The contents of the pipe if possible; None if this is a writing process.
+        """
+        if not self.is_reader:  # The writer-end of the pipe has no access to read!
+            return
+        return pickle.load(self.f)
+
+    def close(self):
+        """Closes the FIFO pipe. When the writer thread closes the pipe, the pipe is removed."""
+        self.f.close()
+        if not self.is_reader:
+            os.remove(self.fname)
+
+def __fetch_contents(_, frame):
+    """Entry point for listener when triggered by internal signal."""
+    pp = PipedProcess(os.getpid())
+    variables = dict()
+    dictionaries = [frame.f_globals, frame.f_locals]
+    # Full options for frame: https://docs.python.org/3/library/inspect.html#types-and-members
+    # f_back, f_builtins, f_code, f_globals, f_lasti, f_llineno, f_locals, f_trace
+    for d in dictionaries:  # Iterate over dictionaries and only keep the pickleable data
+        for k, v in d.items():
+            try:
+                _ = pickle.dumps(v)
+                variables[k] = v
+            except TypeError:  # Can't pickle
+                continue
+    pp.write(variables)
+    pp.close()  # Terminate pipe and cleanup
+
+
+def peek(pid : int) -> dict:
+    """Attempts to fetch contents from given PID.
+    Assumes the process is a Python process with the meeshkan_listener() instansiated.
+
+    :return A dictionary as determined by `__fetch_contents`
+    """
+    os.kill(pid, signal.SIGUSR1)
+    pp = PipedProcess(pid, reader=True)
+    variables = pp.read()
+    pp.close()
+    return variables
+
+
+def meeshkan_listener():
+    # TODO - this probably does not work on Windows, see https://docs.python.org/3/library/signal.html#signal.signal
+    signal.signal(signal.SIGUSR1, __fetch_contents)
