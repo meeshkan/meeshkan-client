28e729156cc0c4b5891b4d40c428096633220afe
Idan Tene <idan.tene@gmail.com>
2018-11-02 16:51:24 +0200

    Initial version.
    Able to fetch globals() and locals() from another python process that also had `meeshkan_listener()` call in it.
    Depends on pickle to send variables. More experimenting with PyTorch and TF is required, to verify we can pull different data.

--- client/__init__.py
+++
@@ -1,9 +0,0 @@
-# TODO - automate the import process for top-level... (using os?)
-import client.api
-import client.config
-import client.job
-import client.logger
-import client.notifiers
-import client.oauth
-import client.scheduler
-import client.service
