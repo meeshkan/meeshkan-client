34966013b3911b5dc135477b8a6e248d87837ea0
Idan Tene <idan.tene@gmail.com>
2018-11-09 19:21:45 +0200

    Merging TokenSource and TokenStore and CloudClient to return response

--- meeshkan/oauth.py
+++ meeshkan/oauth.py
@@ -5,23 +5,23 @@ from typing import Callable, Dict, NewType
 import requests
 
 import meeshkan.exceptions
+import meeshkan.cloud
 
 LOGGER = logging.getLogger(__name__)
 
 Token = NewType("Token", str)
-FetchToken = NewType("FetchToken", Callable[[], Token])
-
 
 class TokenSource(object):
     """
-    Fetch access tokens via `fetch` method. Call `.close()` to close the underlying requests Session!
+    Fetches and caches access authentication tokens via `_fetch_token` method.
+    Call `.close()` to close the underlying requests Session!
     """
-    def __init__(self, auth_url: str, client_id: str, client_secret: str,
-                 build_session: Callable[[], requests.Session]):
+    def __init__(self, auth_url: str, client_id: str, client_secret: str, session: requests.Session):
         self._auth_url = auth_url
         self._client_id = client_id
         self._client_secret = client_secret
-        self._session = build_session()
+        self._session = session
+        self._token = None
 
     def __enter__(self):
         return self
@@ -31,14 +31,10 @@ class TokenSource(object):
 
     @property
     def _payload(self) -> Dict[str, str]:
-        return {
-            'client_id': self._client_id,
-            'client_secret': self._client_secret,
-            'audience': "https://api.meeshkan.io",
-            "grant_type": "client_credentials"
-        }
+        return dict(client_id=self._client_id, client_secret=self._client_secret, audience="https://api.meeshkan.io",
+                    grant_type="client_credentials")
 
-    def fetch_token(self) -> Token:
+    def _fetch_token(self) -> Token:
         LOGGER.debug("Requesting token with payload %s", self._payload)
         resp = self._session.post("https://{url}/oauth/token".format(url=self._auth_url), data=self._payload, timeout=5)
 
@@ -52,21 +48,13 @@ class TokenSource(object):
         LOGGER.error("Failed requesting authentication: status %s, text: %s", resp.status_code, resp.text)
         raise RuntimeError("Failed requesting authentication.")
 
-    def close(self):
-        LOGGER.debug("Closing TokenSource session.")
-        self._session.close()
-
-
-class TokenStore(object):
-    """
-    Caches authentication tokens, fetches new ones via `fetch_token`
-    """
-    def __init__(self, fetch_token: FetchToken):
-        self._token = None
-        self._fetch_token = fetch_token
-
-    def get_token(self, refresh=False):
+    def get_token(self, refresh=False) -> Token:
         if refresh or self._token is None:
             LOGGER.info("Retrieving new authentication token")
             self._token = self._fetch_token()
         return self._token
+
+    def close(self):
+        LOGGER.debug("Closing TokenSource session.")
+        self._session.close()
+
