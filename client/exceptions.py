46d1f4c07700b9e71467d8d25374d60295f42491
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-11-09 14:57:18 +0200

    Remove accidentally added files.

--- client/exceptions.py
+++
@@ -1,7 +0,0 @@
-class Unauthorized(Exception):
-    """Raised when cloud server responds with 401."""
-    def __init__(self, message=""):
-        if len(message) > 0:
-            message = "\n{message}".format(message=message)
-        self.message = "Unauthorized. Check your credentials.{message}".format(message=message)
-
