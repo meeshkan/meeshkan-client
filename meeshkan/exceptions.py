c9d92b336549306b88ecf1867d70ff05122484ba
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-11-09 11:13:06 +0200

    Fix exceptions module.

--- meeshkan/exceptions.py
+++ meeshkan/exceptions.py
@@ -1,12 +1,14 @@
 class UnauthorizedRequestException(Exception):
     """Raised when cloud server responds with 401."""
     def __init__(self, message=""):
-        if len(message) > 0:
+        super().__init__()
+        if message:
             message = f"\n{message}"
         self.message = f"Unauthorized. Check your credentials.{message}"
+
 
 class OldVersionException(Exception):
     """Raised when there exists a new version of meeshkan."""
     def __init__(self):
+        super().__init__()
         self.message = "Update exists - please update meeshkan first."
-
