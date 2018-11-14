ad5d96ec9af9bc316770c528b89d7ac365f60173
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-11-14 08:51:09 +0200

    Poll tasks with cloud client.

--- meeshkan/__main__.py
+++ meeshkan/__main__.py
@@ -77,8 +77,7 @@ def __build_api(config: meeshkan.config.Configuration,
             post_payload=cloud_client.post_payload)
         logging_notifier: meeshkan.notifiers.LoggingNotifier = meeshkan.notifiers.LoggingNotifier()
 
-        task_source = meeshkan.tasks.TaskSource()
-        task_poller = meeshkan.tasks.TaskPoller(task_source)
+        task_poller = meeshkan.tasks.TaskPoller(cloud_client.pop_tasks())
         scheduler = meeshkan.scheduler.Scheduler(task_poller)
         scheduler.register_listener(logging_notifier)
         scheduler.register_listener(cloud_notifier)
--- meeshkan/tasks.py
+++ meeshkan/tasks.py
@@ -3,48 +3,25 @@ Code related to tasks invoked by the cloud.
 """
 import asyncio
 import logging
-from typing import Callable
+from typing import Awaitable, List
 
-import requests
 
 LOGGER = logging.getLogger(__name__)
 
 
-class TaskSource:
-    """
-    Asynchronous task source. Usage as context manager:
-    with task_source:                          # Builds session
-        tasks = await task_source.pop_tasks()  # Closes session when exiting
-    """
-    def __init__(self, build_session: Callable[[], requests.Session] = requests.Session):
-        self._build_session = build_session
-        self._session = None
-
-    def __enter__(self):
-        self.start()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    def start(self):
-        if self._session is not None:
-            raise RuntimeError("Trying to start TaskSource twice before closing.")
-        self._session = self._build_session()
-
-    def close(self):
-        if self._session is None:
-            raise RuntimeError("Trying to close TaskSource without starting.")
-        self._session.close()
-        self._session = None
-
-    async def pop_tasks(self):
-        await asyncio.sleep(0.1)  # TODO: Query server
-        return [{'counter': 0}]
+class Task:
+    def __init__(self, job_id, task):
+        self.job_id = job_id
+        self.task = task
 
 
 class TaskPoller:
-    def __init__(self, task_source):
-        self._task_source = task_source
+    def __init__(self, pop_tasks_coro: Awaitable[List[Task]]):
+        """
+        Polls new tasks from the server.
+        :param pop_tasks: Asynchronous method for fetching new tasks
+        """
+        self._pop_tasks_coro = pop_tasks_coro
 
     async def poll(self, handle_task, delay=10):
         """
@@ -55,12 +32,16 @@ class TaskPoller:
         :return:
         """
         try:
-            with self._task_source:
-                while True:
-                    tasks = await self._task_source.pop_tasks()
+            while True:
+                try:
+                    tasks = await self._pop_tasks_coro  # type: List[Task]
                     for task in tasks:
                         await handle_task(task)
-                    await asyncio.sleep(delay)
+                except Exception as ex:  # pylint:disable=broad-except
+                    if isinstance(ex, asyncio.CancelledError):
+                        raise
+                    LOGGER.exception("Failed fetching or handling new tasks.")
+                await asyncio.sleep(delay)
         except asyncio.CancelledError:
             LOGGER.debug("Polling canceled.")
             raise
