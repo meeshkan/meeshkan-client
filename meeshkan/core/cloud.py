d54a2f32c45105974d2bc0d741052a47d69b2636
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-01-07 15:01:25 +0200

    Notify server of job failure (#99)
    * Notify cloud of failed job.
    
    * More clean-up.
    
    * No need for message.
    
    * Group notifier tests in classes.
    
    * Add test for failure notify.
    
    * Change stderr dir.
    
    * Clean-up.
    
    * More clean-up.

--- meeshkan/core/cloud.py
+++ meeshkan/core/cloud.py
@@ -43,19 +43,15 @@ class CloudClient:
             raise RuntimeError("Can't instantiate a CloudClient without either TokenStore or refresh token")
         self._session = build_session()
 
-
     def __enter__(self):
         return self
-
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
-
     def _post(self, payload: Payload, token: Token = None) -> requests.Response:
         headers = {"Authorization": "Bearer {token}".format(token=token)} if token is not None else None
         return self._session.post(self._cloud_url, json=payload, headers=headers, timeout=5)
-
 
     @staticmethod
     def _check_for_errors(res):
@@ -90,7 +86,6 @@ class CloudClient:
 
         raise RuntimeError("Error posting to server")
 
-
     def _post_gql_payload(self, payload: Payload, retries: int = 1, delay: float = 0.2) -> dict:
         """Post to `cloud_url` with retry: If unauthenticated, fetch a new token and retry the given number of times.
         Checks that the response does not contain any errors, raises error if yes.
@@ -121,10 +116,8 @@ class CloudClient:
 
         raise UnauthorizedRequestException
 
-
     def post_payload(self, payload: Payload) -> None:
         self._post_gql_payload(payload)
-
 
     def get_new_token(self, refresh_token: str) -> Token:
         query = "query GetToken($refresh_token: String!) { token(refreshToken: $refresh_token) { access_token } }"
@@ -132,7 +125,6 @@ class CloudClient:
         res = self._post(payload)
         CloudClient._check_for_errors(res)
         return res.json()['data']['token']['access_token']
-
 
     def _upload_file(self, method, url, headers, file):
         """Uploads a file to given URL with method and headers
@@ -145,7 +137,6 @@ class CloudClient:
             LOGGER.error("Error on file upload: %s", res.text)
             raise RuntimeError("File upload failed with status code {status_code}".format(status_code=res.status_code))
         LOGGER.debug("Uploading file, got response %s", res)
-
 
     def post_payload_with_file(self, file: Union[str, Path], download_link=False) -> Optional[str]:
         """Uploads a file to `cloud_url`, fetching a download link if needed. All without retry.
@@ -181,7 +172,6 @@ class CloudClient:
         self._upload_file(method=upload_method, url=upload_url, headers=upload_headers, file=file)
         return download_url
 
-
     def notify_service_start(self):
         """Build GraphQL query payload and send to server when service is started
         Schema of job_input MUST match with the server schema
@@ -192,7 +182,6 @@ class CloudClient:
         input_dict = {"version": __version__}
         payload = {"query": mutation, "variables": {"in": input_dict}}
         self.post_payload(payload)
-
 
     def pop_tasks(self) -> List[Task]:
         """Build GraphQL query payload and send to server for new tasks
@@ -213,7 +202,6 @@ class CloudClient:
 
         tasks = [build_task(json_task) for json_task in tasks_json]
         return tasks
-
 
     def close(self):
         LOGGER.debug("Closing CloudClient session")
