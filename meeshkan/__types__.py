7c46007881db4465cb04ba24cca7cc7c0642144b
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-01-10 10:33:10 +0200

    Restore __types__.

--- meeshkan/__types__.py
+++ meeshkan/__types__.py
@@ -1,7 +1,6 @@
 """Module to define different types used through Meeshkan codebase"""
-from typing import NewType, Dict, Any, List
+from typing import NewType, Dict, Any, List, Tuple
 import time
-
 
 class ScalarIndexPairing(object):
     """Represents a coupling between a scalar value and an index. If no index is provided, time.monotonic() is used
@@ -10,8 +9,7 @@ class ScalarIndexPairing(object):
         self.value = value
         self.idx = idx if idx is not None else time.monotonic()
 
-
 Token = NewType("Token", str)
-Payload = Dict
+Payload = Dict[str, Any]
 # Keys are scalar names, value is a list of tuples, indicating wall time and value for that scalar
-HistoryByScalar = Dict
+HistoryByScalar = Dict[str, List[ScalarIndexPairing]]
