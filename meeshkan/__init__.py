3b9e7877bafd0061a24802eaa3d107d69074b751
Idan Tene <idan.tene@gmail.com>
2019-01-14 16:29:44 +0200

    Removes 'notifications' from the exposed modules...

--- meeshkan/__init__.py
+++ meeshkan/__init__.py
@@ -1,21 +1,28 @@
 from .__version__ import __version__  # Conform to PEP-0396
 
-from .core import *  # pylint: disable=wildcard-import
 from . import core
-from .api import *  # pylint: disable=wildcard-import
+from .core import *  # pylint: disable=wildcard-import
 from . import api
+from .api import *  # pylint: disable=wildcard-import
+from . import agent
+from .agent import *  # pylint: disable=wildcard-import
+from . import notifications
+from .notifications import *  # pylint: disable=wildcard-import
+
 from . import exceptions
 from . import sagemaker
 from .__utils__ import save_token
-from . import agent
-from .agent import *
 
 
 # Only make the following available by default
-__all__ = ["__version__", "exceptions", "config"]
+__all__ = ["__version__", "exceptions", "config", "save_token"]
 __all__ += api.__all__
 __all__ += agent.__all__
+__all__ += core.__all__
+__all__ += notifications.__all__
 
-del core  # Clean-up (make `meeshkan.core` unavailable)
+# Clean-up (make `meeshkan.core`, etc unavailable)
+del core
 del api
 del agent
+del notifications
