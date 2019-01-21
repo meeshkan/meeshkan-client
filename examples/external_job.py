e13775eea5882d64a7ef79ea0e3eeeb614e2c0b9
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-01-21 11:02:31 +0200

    Use meeshkan as function decorator.

--- examples/external_job.py
+++ examples/external_job.py
@@ -3,13 +3,33 @@ import time
 import meeshkan
 
 
+def my_loop():
+    for i in range(10):
+        meeshkan.report_scalar("i", i)
+        time.sleep(2)
+
+
+@meeshkan.as_job(job_name="test-job", report_interval=10)
 def main():
+    my_loop()
+
+
+# Besides decoration, you can also use meeshkan jobs as context managers:
+def main_with_context_manager():
     job = meeshkan.create_external_job(name="my-job", poll_interval=10)
     with job:
-        for i in range(10):
-            meeshkan.report_scalar("i", i)
-            time.sleep(2)
+        my_loop()
 
 
 if __name__ == '__main__':
+
+    meeshkan_agent_was_running = meeshkan.is_running()
+
+    if not meeshkan_agent_was_running:
+        # time.sleep(2)
+        meeshkan.start()
+        # time.sleep(2)
     main()
+
+    if not meeshkan_agent_was_running:
+        meeshkan.stop()
--- meeshkan/api/external_job.py
+++ meeshkan/api/external_job.py
@@ -1,9 +1,10 @@
+from functools import wraps
 import os
 from typing import Optional
 import uuid
 from ..core.service import Service
 
-__all__ = ["create_external_job"]
+__all__ = ["create_external_job", "as_job"]
 
 
 class ExternalJobWrapper:
@@ -17,6 +18,26 @@ class ExternalJobWrapper:
     def __exit__(self, exc_type, exc_val, exc_tb):
         # Unregister active job
         unregister_external_job(self.job_id)
+
+
+def tags(tag_name):
+    def tags_decorator(func):
+        @wraps(func)
+        def func_wrapper(name):
+            return "<{0}>{1}</{0}>".format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
+
+
+def as_job(job_name, report_interval):
+    def job_decorator(func):
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            job = create_external_job(name=job_name, poll_interval=report_interval)
+            with job:
+                func(*args, *kwargs)
+        return func_wrapper
+    return job_decorator
 
 
 def create_external_job(name: str, poll_interval: Optional[float] = None) -> ExternalJobWrapper:
