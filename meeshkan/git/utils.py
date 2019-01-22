c32d9070f3ca01fc18a5bb51a50c269e4b5b882b
Idan Tene <idan.tene@gmail.com>
2019-01-22 12:16:02 +0200

    WIP Initial Git integration

---
+++ meeshkan/git/utils.py
@@ -0,0 +1,9 @@
+"""Provides utilities for pulling and parsing Git commits"""
+from typing import List
+from github import Github
+
+__all__ = []  # type: List[str]
+
+def pull_commit(repo: str, commit: str):
+    pass
+
