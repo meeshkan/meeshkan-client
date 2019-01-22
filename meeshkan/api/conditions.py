7c860bc589ddb9955e5f25edca45bc128c66ab4d
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-01-22 16:08:16 +0200

    Update documentation for library methods (#137)
    * Add documentation.
    
    * Remove unused rst.
    
    * Do not export Api.
    
    * Raise TypeError instead of ValueError.

--- meeshkan/api/conditions.py
+++ meeshkan/api/conditions.py
@@ -7,16 +7,39 @@ __all__ = ["add_condition"]
 
 
 def add_condition(*vals, condition, only_reported=False):
-    """Adds a condition to send notification for given values when condition holds
+    """
+    Adds a condition to send notification when scalars fulfill a condition. Requires Meeshkan agent
+    to be running and aware of the job context as for :func:`meeshkan.report_scalar`.
 
-    :param vals: A list of value names to monitor
-    :param condition: A callable accepting as many arguments as listed values, and returns whether the notification
-        condition has been met.
-    :param only_reported: Flag whether or not to report all scalars in a job, or just the ones relevant to the condition
-        (False by default -> reports all scalars in the job)
+    Example::
+
+        # Add a condition to notify when training loss is less than 0.8
+        meeshkan.add_condition("train_loss", lambda v: v < 0.8)
+
+        # Add another condition to notify when `val_loss` and `val_acc` are smaller and greater
+        # than given values, respectively
+        meeshkan.add_condition("val_loss", "val_acc", lambda loss, acc: loss < 0.5 and acc > 0.95)
+
+        for epoch in range(EPOCHS):
+            # Compute `train_loss`
+            train_loss = ...
+            # Report the value to the agent.
+            # If the added condition is fulfilled, notification is sent.
+            meeshkan.report_scalar("train_loss", train_loss)
+
+            # Report validation results
+            if epoch % VALIDATION_INTERVAL == 0:
+                val_loss = ...
+                val_acc = ...
+                meeshkan.report_scalar("val_loss", val_loss, "val_acc", val_acc)
+
+    :param vals: List of scalar names to include in the condition definition.
+    :param condition: A callable accepting as many arguments as listed values and returning boolean.
+    :param only_reported: Report all scalars in a job if True, only report the ones \
+    relevant to the condition if False. Defaults to False.
     """
     if not vals:
-        raise RuntimeError("No arguments given for condition!")
+        raise TypeError("No arguments given for condition!")
 
     pid = os.getpid()
     with Service.api() as proxy:
--- meeshkan/api/scalars.py
+++ meeshkan/api/scalars.py
@@ -5,14 +5,33 @@ from ..exceptions import JobNotFoundException
 __all__ = ["report_scalar"]
 
 
-def report_scalar(val_name, value, *vals) -> bool:
-    """Reports scalars to the meeshkan service API
+def report_scalar(val_name: str, value: float, *vals) -> bool:
+    """
+    Reports scalars to the Meeshkan agent. Reported scalars are included in the sent notifications.
+
+    Requires Meeshkan agent to be running and aware of the
+    job context. The job context can be defined in multiple ways:
+
+        1. By submitting the script or notebook for execution to the agent with ``meeshkan submit``.
+        2. By decorating a function with :func:`meeshkan.as_blocking_job`
+        3. By using the job context manager :func:`meeshkan.create_blocking_job`
+
+    Example of ``train.py`` script submitted with ``meeshkan submit --name my-job train.py``::
+
+        import meeshkan
+
+        EPOCHS = 10
+
+        for epoch in range(EPOCHS):
+            # Compute loss
+            loss = ...
+            # Report loss to the Meeshkan agent
+            meeshkan.report_scalar("loss", loss)
 
     :param val_name: The name of the scalar to report
     :param value: The value of the scalar
-    :param vals: any additional value_name, value to add.
-    :param condition: A callable that accepts all scalars registered in this statement, and return True or False,
-        indicating whether a notification is required.
+    :param vals: Any additional (val_name, value) pairs to add.
+    :return bool: True if job was found, False if not.
     """
     if len(vals) % 2:  # Invalid number of additional scalar arguments given
         raise RuntimeError("Invalid number of arguments given - did you forget a name/value?")
