def166a1647399b1d5ac01cc3e79c0267c044429
Idan Tene <idan.tene@gmail.com>
2019-01-25 18:55:42 +0200

    Fixes tests

--- tests/test_git.py
+++ tests/test_git.py
@@ -8,7 +8,7 @@ from .utils import TempCredentialsFile, wait_for_true
 
 import meeshkan
 from meeshkan.core.service import Service
-import meeshkan.git.utils as mg
+from meeshkan.git.utils import submit_git, GitRunner
 
 CLIENT_REPO = "Meeshkan/meeshkan-client"
 FIRST_VERSION_COMMIT = "cee35e9"  # v0.0.1 psuedo-release
@@ -17,7 +17,7 @@ FIRST_OFFICIAL_RELEASE_BRANCH = "release-v-0.1.0"  # first official release
 
 @pytest.fixture
 def clean():
-    def remove_tempdir_from_gitrunner(gitrunner: mg.GitRunner):
+    def remove_tempdir_from_gitrunner(gitrunner: GitRunner):
         shutil.rmtree(gitrunner.target_dir)
     return remove_tempdir_from_gitrunner
 
@@ -28,23 +28,23 @@ def test_git_verify():
         return "fake_address" if git_exists else None
 
     with mock.patch("shutil.which", fake_which):
-        with pytest.raises(mg.GitRunner.GitException):
-            mg.GitRunner._verify_git_exists()
+        with pytest.raises(GitRunner.GitException):
+            GitRunner._verify_git_exists()
         git_exists = True
-        mg.GitRunner._verify_git_exists()
+        GitRunner._verify_git_exists()
 
 
 def test_git_access_token():
     with TempCredentialsFile(refresh_token="fake") as tmpfile:
-        with pytest.raises(mg.GitRunner.GitException):
-            mg.GitRunner._git_access_token(tmpfile.file), "Default test credentials do not contain a github token!"
+        with pytest.raises(GitRunner.GitException):
+            GitRunner._git_access_token(tmpfile.file), "Default test credentials do not contain a github token!"
         tmpfile.update(git_token="abc")
-        assert mg.GitRunner._git_access_token(tmpfile.file) == "abc"
+        assert GitRunner._git_access_token(tmpfile.file) == "abc"
 
 
 def test_gitrunner_init(clean):
     repo = "foobar"
-    gitrunner = mg.GitRunner(repo=repo)
+    gitrunner = GitRunner(repo=repo)
     git_token = meeshkan.config.CREDENTIALS.git_access_token
     try:
         assert gitrunner.url == "https://{token}:x-oauth-basic@github.com/{repo}".format(token=git_token, repo=repo)
@@ -53,7 +53,7 @@ def test_gitrunner_init(clean):
 
 
 def test_gitrunner_pull_branch(clean):
-    gitrunner = mg.GitRunner(repo=CLIENT_REPO)
+    gitrunner = GitRunner(repo=CLIENT_REPO)
     gitrunner.pull_repo(branch=FIRST_OFFICIAL_RELEASE_BRANCH)
     version_fname = os.path.join(gitrunner.target_dir, "meeshkan/__version__.py")
     try:
@@ -65,7 +65,7 @@ def test_gitrunner_pull_branch(clean):
 
 
 def test_gitrunner_pull_commit(clean):
-    gitrunner = mg.GitRunner(repo=CLIENT_REPO)
+    gitrunner = GitRunner(repo=CLIENT_REPO)
     gitrunner.pull_repo(commit_sha=FIRST_VERSION_COMMIT)
     version_fname = os.path.join(gitrunner.target_dir, "client/__version__.py")
     try:
@@ -77,7 +77,7 @@ def test_gitrunner_pull_commit(clean):
 
 
 def test_gitrunner_pull_branch_and_commit(clean):
-    gitrunner = mg.GitRunner(repo=CLIENT_REPO)
+    gitrunner = GitRunner(repo=CLIENT_REPO)
     gitrunner.pull_repo(branch=FIRST_OFFICIAL_RELEASE_BRANCH, commit_sha=FIRST_VERSION_COMMIT)
     version_fname = os.path.join(gitrunner.target_dir, "client/__version__.py")
     try:
